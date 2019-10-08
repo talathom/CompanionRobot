6595512ea334475e6d19f18c559cc34ea06f9df7
talathom <tp22tala@siena.edu>
2019-10-07 22:31:37 -0400

    Moved all magic numbers to constants

--- src/GenerateMarker.py
+++ src/GenerateMarker.py
@@ -9,16 +9,22 @@ import sys
 import cv2
 from cv2 import aruco
 
+PIXELS_PER_INCH = 96
+ARG_LENGTH = 3 # Required number of arguments no more, no less
+MARKER_ID_UPPER_BOUND = 249 #Highest ID in AR dict
+MARKER_ID_LOWER_BOUND = 0 # Lowest ID in AR dict
+MIN_PIXELS = 6 # Minimum number of pixels is 6 since we're using a 6x6 Dictionary
+
 #Import AR Dictionary
 ar_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
 #Verify correct num of args
-if len(sys.argv) != 3:
+if len(sys.argv) != ARG_LENGTH:
     print("Invalid args - Usage: marker_id sidelengthinches")
 else:
     #Verify correct format
     try:
         marker_id = int(sys.argv[1])
-        if marker_id > 249 or marker_id < 0:
+        if marker_id > MARKER_ID_UPPER_BOUND or marker_id < MARKER_ID_LOWER_BOUND:
             sys.exit(1) #Throw exception on bad ID
     except:
         print("Invalid Marker ID, Valid ID 0-249")
@@ -26,8 +32,8 @@ else:
         inches = int(sys.argv[2])
     except:
         print("Inches arg must be an integer")
-    marker_size = inches * 96 # Get size in pixels
-    if marker_size < 6:
+    marker_size = inches * PIXELS_PER_INCH # Get size in pixels
+    if marker_size < MIN_PIXELS:
         print("Cannot create 6x6 Marker with a size of 5x5 or smaller")
     else:
         #Write to file
